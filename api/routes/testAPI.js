b419c9e9002c13f2446290196ba95115cb9809c3
Muhammad Zaeem <zaeem@MacBook-Air-226.local>
2023-08-13 18:27:39 -0400

    remove unnecessary test method

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+router.use(express.json())
+
 router.post("/", function(req, res, next) {
     console.log(req.body)
     res.send("API is working properly POST");
