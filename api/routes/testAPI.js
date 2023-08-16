b8f6cb97075f93d01146b1072b4cf42a6e08745a
Muhammad Zaeem <zaeem@MacBook-Air-226.local>
2023-08-15 23:18:04 -0400

    fetch and display movie data when clicking submit

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,49 +1,68 @@
 var express = require("express");
 var router = express.Router();
+const axios = require("axios"); // Import the axios library
 
-router.use(express.json())
-
+router.use(express.json());
 
 function stringOfProviders(providerList) {
-    let providerArray = []
-    providers = { "Netflix": 8, "Hulu": 15, "AppleTV": 350}
+  let providerArray = [];
+  providers = { "Netflix": 8, "Hulu": 15, "Apple TV": 350 };
 
-    for (const provider of providerList) {
-        if (provider in providers) {
-            providerArray.push(providers[provider]);
-        }
-        
+  for (const provider of providerList) {
+    if (provider in providers) {
+      providerArray.push(providers[provider]);
     }
-    let resultString = providerArray.join("|");
-    return resultString;
-
+  }
+  let resultString = providerArray.join("|");
+  return resultString;
 }
 
+function stringOfGenres(genreList) {
+    let genereArray = [];
+    genres = { "Action": 28, "Adventure": 12, "Animation": 16, "Comedy": 35, "Crime": 80, "Documentary": 99, "Drama": 18, "Family": 10751,
+                "Fantasy": 14, "History": 36, "Horror": 27, "Music": 10402, "Mystery": 9648, "Romance": 10749, "Science Fiction": 878, 
+                "TV Movie": 10770, "Thriller": 53, "War": 10752, "Western": 32 };
 
+    for (const genre of genreList) {
+        if (genre in genres) {
+        genreArray.push(genres[genre]);
+        }
+    }
+    let resultString = genereArray.join("|");
+    return resultString;
+}
 
 async function getMovieData(providerIds) {
+    
     const options = {
-        method: 'GET',
         headers: {
         accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTFhNmEyNjIxOTIxNDJkODM3YTYwN2NjOTE5NjdkNyIsInN1YiI6IjY0ZDk0OTU1NjNhYWQyMDEwMDIyYjIyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HY1G3T5diE0Td6wZ_e_whWosholUmqBqlURMnXpkbow'
-      }
-      };
-      
-    fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&watch_region=US&with_original_language=en&with_watch_providers=${providerIds}`, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+        }
+    };
+
+    try {
         
+        const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_count.desc&vote_average.gte=7.5&vote_count.gte=100&watch_region=US&with_original_language=en&with_watch_providers=${providerIds}`, options);
+        return response.data;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
 
-router.post("/", function(req, res, next) {
-    let providerIds = stringOfProviders(req.body.data)
-    console.log(providerIds)
-    getMovieData(providerIds)
-    res.send("API is working properly POST");
+router.post("/", async function(req, res, next) {
+    let providerIds = stringOfProviders(req.body.data);
+    const movieData = await getMovieData(providerIds);
+    res.json(movieData.results); // Send the movie data as JSON response
+
+    movieData.results.forEach(item => {
+        console.log("Original Title:", item.original_title);
+        console.log("-------------------------------");
+      });
+    // res.send("API is working properly POST");
+
+
 });
-
-
 
 module.exports = router;
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,7 +1,11 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-const Form = () => {
-    const handleSubmit = (e) => {
+
+function Form(){
+    const [movieData, setMovieData] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         const form = e.target;
@@ -12,17 +16,26 @@ const Form = () => {
         for (const key of formData.keys()) {
             selectedServices.push(key);
         }
+        if (selectedServices.length === 0) {
+            setErrorMessage("Select a service before clicking submit");
+            return;
+        }
+        else {
+            setErrorMessage("");
+        }
 
         //making request to server
-        fetch("http://localhost:9000/testAPI", {
+        const response = await fetch("http://localhost:9000/testAPI", {
             method: 'POST',
             headers: {
             'Content-Type': 'application/json'
             },
             body: JSON.stringify({ data: selectedServices }) 
-        })
-        .then(res => res.text())
-        .then(res => console.log({ apiResponse: res }));
+        });
+        if (response.ok) {
+            const data = await response.json();
+            setMovieData(data); // Update state with movie data
+          } 
 
     };
 
@@ -31,16 +44,31 @@ const Form = () => {
             <form method="post" onSubmit={handleSubmit}>
                 <p>Select Streaming Service:</p>
                 <label>
-                    Netflix: <input type="checkbox" name="Netflix"/>
+                    Netflix: <input type="checkbox" name="Netflix" />
                 </label>
                 <label>
-                    Hulu: <input type="checkbox" name="Hulu"/>
+                    Hulu: <input type="checkbox" name="Hulu" />
                 </label>
                 <label>
-                    AppleTV: <input type="checkbox" name="AppleTV"/>
+                    Apple TV: <input type="checkbox" name="Apple TV" />
                 </label>
                 <button type="submit">Submit form</button>
+                {errorMessage && <p>{errorMessage}</p>}
             </form>
+
+            <div className='flex-container'>
+                {movieData.map((item) =>
+                <div className="movie_item">
+                    <img src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`} />
+                    <div className="movie_name">
+                        {item.original_title ? item.original_title : item.original_name}
+
+                    </div>
+                </div>
+                )}
+            </div>
+            
+
         </>
     );
 };
